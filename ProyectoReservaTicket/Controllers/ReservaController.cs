0d7b1cbbf09d78644cfb1c622bde092cc0dd3798
Catherine <00dahy@gmail.com>
2020-12-01 16:10:35 -0300

    Se creó el controlador de Reserva y una Vista para listar los elementos

---
+++ ProyectoReservaTicket/Controllers/ReservaController.cs
@@ -0,0 +1,19 @@
+﻿using ProyectoReservaTicket.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoReservaTicket.Controllers
+{
+    public class ReservaController : Controller
+    {
+        // GET: Reserva
+        private ReservaTicketEntities db = new ReservaTicketEntities();
+        public ActionResult Index()
+        {
+            return View(db.Reserva.ToList());
+        }
+    }
+}
