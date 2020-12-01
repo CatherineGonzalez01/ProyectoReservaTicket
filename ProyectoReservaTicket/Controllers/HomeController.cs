a6e5acbdfe4422a01310c940f20af694cd7b9492
Catherine <00dahy@gmail.com>
2020-12-01 12:32:55 -0300

    Se ha creado la acción para dar de alta las consultas y su respectiva vista

--- ProyectoReservaTicket/Controllers/HomeController.cs
+++ ProyectoReservaTicket/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProyectoReservaTicket.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,7 @@ namespace ProyectoReservaTicket.Controllers
 {
     public class HomeController : Controller
     {
+        private ReservaTicketEntities db = new ReservaTicketEntities();
         public ActionResult Index()
         {
             return View();
@@ -19,7 +21,27 @@ namespace ProyectoReservaTicket.Controllers
 
             return View();
         }
+        public ActionResult CreateConsulta()
+        {
+            return View();
+        }
 
+        // POST: Eventos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateConsulta([Bind(Include = "consultaID,Nombre,Apellido,Telefono,consulta")] Consultas consulta )
+        {
+            if (ModelState.IsValid)
+            {
+                db.Consultas.Add(consulta);
+                db.SaveChanges();
+                return RedirectToAction("Contact");
+            }
+
+            return View(consulta);
+        }
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
