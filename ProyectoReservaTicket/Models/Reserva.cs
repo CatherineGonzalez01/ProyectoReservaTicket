1d1100d1314aada1c1003610865756d5e08f0345
Catherine <00dahy@gmail.com>
2020-12-01 16:45:28 -0300

    Se modificaron las tablas de las vistas Reserva y Evento

--- ProyectoReservaTicket/Models/Reserva.cs
+++ ProyectoReservaTicket/Models/Reserva.cs
@@ -11,17 +11,23 @@ namespace ProyectoReservaTicket.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Reserva
     {
         public int ReservaID { get; set; }
+        [Display(Name = "Fecha Reserva")]
         public System.DateTime FechaReserva { get; set; }
+        [Display(Name = "Evento")]
         public int EventoID { get; set; }
+        [Display(Name = "Nombre y Apellido")]
         public string NombreAepllido { get; set; }
         public string Direccion { get; set; }
         public string Telefono { get; set; }
         public string Email { get; set; }
+        [Display(Name = "Cantidad Ticket")]
         public int CantidadTicket { get; set; }
+        [Display(Name = "Total a Pagar")]
         public decimal TotalPagar { get; set; }
     
         public virtual Evento Evento { get; set; }
