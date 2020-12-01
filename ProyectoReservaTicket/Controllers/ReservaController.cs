5746966b71ac1d3a362d1e1dd6cdc0e28ff80a04
Catherine <00dahy@gmail.com>
2020-12-01 17:04:54 -0300

    Se creó la acción para crear Reserva

--- ProyectoReservaTicket/Controllers/ReservaController.cs
+++ ProyectoReservaTicket/Controllers/ReservaController.cs
@@ -15,5 +15,28 @@ namespace ProyectoReservaTicket.Controllers
         {
             return View(db.Reserva.ToList());
         }
+
+        // GET: Eventos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Eventos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ReservaID,FechaReserva,EventoID,NombreAepllido,Direccion,Telefono,Email,CantidadTicket,TotalPagar")] Reserva reserva)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Reserva.Add(reserva);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(reserva);
+        }
     }
 }
