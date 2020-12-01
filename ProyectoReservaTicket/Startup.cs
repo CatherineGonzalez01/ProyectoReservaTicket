3ae27c7ff22cb659f15e75d241820ac242e2e49e
Catherine <00dahy@gmail.com>
2020-12-01 10:35:06 -0300

    Add project files.

---
+++ ProyectoReservaTicket/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProyectoReservaTicket.Startup))]
+namespace ProyectoReservaTicket
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
